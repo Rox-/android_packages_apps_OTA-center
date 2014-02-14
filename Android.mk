4d0ddcf0c091599b4714a5d95ff8895b1a577d52
Robert Rozic <rooobertinhooo@gmail.com>
2014-02-14 19:13:14 +0100

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -5,7 +5,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_PACKAGE_NAME := TeamCanjicaCenter
+LOCAL_PACKAGE_NAME := teamcanjicaCenter
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
